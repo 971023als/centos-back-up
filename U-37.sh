e5c6fe8cd970ca01d2f740ed8f6168b139a19322
971023als <971023als@naver.com>
2023-02-02 13:27:39 +0900

    ok

--- U-37.sh
+++ U-37.sh
@@ -35,19 +35,6 @@ else
     echo "httpd.conf file not found in /[Apache_home]/conf/"
 fi
 
-# 사용자에게 디렉토리 경로를 입력하라는 메시지 표시
-echo "Enter the path of the directory where you want to set user authentication:"
-read -r dir_path
-
-# 지정된 디렉토리에 .htaccess 파일 생성
-touch $dir_path/.htaccess
-
-# .htaccess 파일에 인증 설정 추가
-echo "AuthName \"Authenticate Directory Users\"" >> $dir_path/.htaccess
-echo "AuthType Basic" >> $dir_path/.htaccess
-echo "AuthUserFile /usr/local/apache/test/.auth" >> $dir_path/.htaccess
-echo "Require valid-user" >> $dir_path/.htaccess
-
 
 # Apache 데몬을 재시작하여 변경된 설정 적용
 sudo service apache2 restart
