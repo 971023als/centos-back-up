24b4b72f148df99943e6d75fb0b1b4cc701102db
971023als <971023als@naver.com>
2023-02-03 20:03:55 +0900

    ok

--- U-72.sh
+++ U-72.sh
@@ -25,20 +25,22 @@ TMP1=`SCRIPTNAME`.log
 > $TMP1 
 
 
-<<<<<<< HEAD
 
-=======
-# 원본 rsyslog.conf 파일 백업
+# Backup the original file
 cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
 
-# rsyslog.conf 파일 수정
-echo "*.info;mail.none;authpriv.none;cron.none /var/log/messages" > /etc/rsyslog.conf
-echo "authpriv.* /var/log/secure" > /etc/rsyslog.conf
-echo "mail.* /var/log/maillog" > /etc/rsyslog.conf
-echo "cron.* /var/log/cron" > /etc/rsyslog.conf
-echo "*.alert /dev/console" > /etc/rsyslog.conf
-echo "*.emerg *" > /etc/rsyslog.conf
->>>>>>> 7804081e626ba4c913c4b2a4dcf6e22bd8f046a3
+# Replace the original file with the new configuration
+cat > /etc/rsyslog.conf << EOF
+*.info;mail.none;authpriv.none;cron.none /var/log/info.log
+authpriv.* /var/log/secure.log
+mail.* /var/log/maillog
+cron.* /var/log/cron
+*.alert /dev/console
+*.emerg *
+EOF
+
+# Restart the rsyslog service
+systemctl restart rsyslog
 
 
 cat $result
