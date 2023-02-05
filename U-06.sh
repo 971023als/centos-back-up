fcb289b4ac82b5db4916cd63cb976fd1180e78bc
971023als <971023als@naver.com>
2023-02-05 11:49:01 +0900

    ok

--- U-06.sh
+++ U-06.sh
@@ -20,17 +20,6 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-# specify the user to replace the owner with
-new_owner=root
-
-# find files and directories with nonexistent owners and groups
-results=$(find / \( -nouser -o -nogroup \) -print 2>/dev/null)
-
-# loop through each item in the results
-for item in $results; do
-  # change the owner to the specified user
-  sudo chown $new_owner:$new_owner "$item"
-done
 
 cat $result
 
