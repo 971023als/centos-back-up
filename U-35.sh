0ba55c4072221906f08fd953a0999b49478e3d6e
971023als <971023als@naver.com>
2023-02-07 16:05:44 +0900

    is

--- U-35.sh
+++ U-35.sh
@@ -20,11 +20,12 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
+
 # Apache 구성 파일 정의
 file="/etc/httpd/conf/httpd.conf"
 
-# "AllowOverrideNone"을 "AllowOverride AuthConfig"로 바꿉니다
-sed -i 's/Options Indexes/Options/g' $file
+# "Options Indexes"을 "Options "로 바꿉니다
+sudo sed -i 's/Options Indexes/Options/g' $file
 
 
 
