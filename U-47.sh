8df84fba72ef738b225bf9be422bdadc24528609
971023als <971023als@naver.com>
2023-02-07 15:24:26 +0900

    is

--- U-47.sh
+++ U-47.sh
@@ -20,11 +20,8 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-# 최대 암호 사용 기간(일)
-max_age=90
+sudo sed -i "s/^PASS_MAX_DAYS.*/PASS_MAX_DAYS\ 90/" /etc/login.defs
 
-# /etc/login.defs에서 최대 암호 사용 기간 설정
-sudo sed -i "s/^\(PASS_MAX_DAYS\s*\).*/\1$max_age/" /etc/login.defs
 
 cat $result
 
