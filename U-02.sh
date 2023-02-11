908207c0dc901b6320003263e16957f3dc18b91b
971023als <971023als@naver.com>
2023-02-12 04:38:08 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -19,8 +19,10 @@ BAR
 # login.defs 파일에서 PASS_MAX_DAYS 값을 가져옵니다
 LOGIN_DEFS_FILE="/etc/login.defs"
 
-# "PASS_MIN_LEN    5"을 "PASS_MIN_LEN    8"로 바꿉니다
-sed -i 's/PASS_MIN_LEN    5/PASS_MIN_LEN    8/g' $LOGIN_DEFS_FILE
+# 전에 있던 PASS_MIN_LEN 값을 #PASS_MIN_LEN
+sed -i 's/PASS_MIN_LEN/#PASS_MIN_LEN/g' $LOGIN_DEFS_FILE
+
+echo "PASS_MIN_LEN 8" >> "$LOGIN_DEFS_FILE"
 
 PAM_FILE="/etc/pam.d/system-auth"
 EXPECTED_OPTIONS="password requisite pam_cracklib.so try_first_pass restry=3 minlen=8 lcredit=-1 ucredit=-1 dcredit=-1 ocredit=-1"
