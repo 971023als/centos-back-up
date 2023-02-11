f1d63d564c0992d678485407e05af4e6d6b444ae
971023als <971023als@naver.com>
2023-02-12 03:47:58 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -18,11 +18,9 @@ BAR
 
 DEF_FILE="/etc/login.defs"
 
-pass=8
+echo "PASS_MIN_LEN 8" >> "$DEF_FILE"
 
-echo "PASS_MIN_LEN $pass" >> "$DEF_FILE"
-
-PAM_FILE="/etc/pam.d/system-auth"
+PAM_FILE="/etc/pam.d/common-auth"
 EXPECTED_OPTIONS="password requisite pam_cracklib.so try_first_pass restry=3 minlen=8 lcredit=-1 ucredit=-1 dcredit=-1 ocredit=-1"
 
 echo ""$EXPECTED_OPTIONS"" >> "$PAM_FILE"
