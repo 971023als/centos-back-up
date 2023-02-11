b9651fee37230a7ed16ad155c2578fff42ec2e68
971023als <971023als@naver.com>
2023-02-11 22:58:09 +0900

    ok

--- U-61.sh
+++ U-61.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
- 
-
 . function.sh
  
-
 BAR
 
 CODE [U-61] ftp 서비스 확인
@@ -24,6 +21,12 @@ TMP1=`SCRIPTNAME`.log
 
 > $TMP1 
 
+# FTP 서비스 중지
+sudo service ftp stop
+
+# FTP 서비스 사용 안 함
+sudo service ftp disable
+
 # vsftpd 서비스를 중지합니다:
 sudo service vsftpd stop
 
@@ -35,6 +38,7 @@ sudo service proftp stop
 
 # proftp를 시작하지 않도록 설정합니다
 sudo /etc/rc.d/init.d/proftp stop
+
 
 cat $result
 
